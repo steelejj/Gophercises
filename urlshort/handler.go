66188d3a6e727440e4da982c58a994f4f9bf7b14
James Steele <jsteele@zushealth.com>
2025-06-30 17:42:44 -0400

    urlshortener working

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -1,8 +1,11 @@
 package urlshort
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -26,6 +29,11 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 	}
 }
 
+type YamlPathURL struct {
+	Path string `yaml:"path"`
+	Url  string `yaml:"url"`
+}
+
 // YAMLHandler will parse the provided YAML and then return
 // an http.HandlerFunc (which also implements http.Handler)
 // that will attempt to map any paths to their corresponding
@@ -34,15 +42,31 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 //
 // YAML is expected to be in the format:
 //
-//     - path: /some-path
-//       url: https://www.some-url.com/demo
+//   - path: /some-path
+//     url: https://www.some-url.com/demo
 //
 // The only errors that can be returned all related to having
 // invalid YAML data.
 //
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
-// func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-// 	// TODO: Implement this...
-// 	return nil, nil
-// }
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	var yamlData []YamlPathURL
+	err := yaml.Unmarshal(yml, &yamlData)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse yaml data")
+	}
+
+	slog.Info("running map handling: ", slog.Any("paths", yamlData))
+	return func(w http.ResponseWriter, r *http.Request) {
+		for _, redirectMap := range yamlData {
+			if redirectMap.Path == r.URL.Path {
+				http.Redirect(w, r, redirectMap.Url, 301)
+				break
+			}
+		}
+		fallback.ServeHTTP(w, r)
+		return
+
+	}, nil
+}
