7ace0d07ba213c0123b112eca4a4e532a70baaa8
James Steele <jsteele@zushealth.com>
2025-07-01 07:52:53 -0400

    working with file inputs

--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -12,10 +13,21 @@ func main() {
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
-	pathsToUrls := map[string]string{
-		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	jsonContent, err := os.ReadFile("urlshort/resources/redirects.json")
+	if err != nil {
+		fmt.Errorf("failed to read json file", err)
 	}
+
+	var pathsToUrls map[string]string
+
+	if err := json.Unmarshal(jsonContent, &pathsToUrls); err != nil {
+		fmt.Errorf("failed to parse json content", err)
+	}
+
+	// pathsToUrls := map[string]string{
+	// 	"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+	// 	"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	// }
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
 	// Build the YAMLHandler using the mapHandler as the
